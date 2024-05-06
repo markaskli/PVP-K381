662096da283a3e0d1c72e75004a1c87cea981447
Markas Klimovas <markasklimovas@gmail.com>
2024-05-06 23:32:13 +0300

    Add endpoints for CRD operations for product

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using API.Models.DTOs.Product;
+using API.Services.ProductService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetProductDTO>> GetById(int id)
+        {
+            try
+            {
+                var product = await _productService.GetProductById(id);
+                if (product != null)
+                {
+                    return Ok(product);
+                }
+
+                return NotFound(new ProblemDetails() { Detail = "Product with specified ID does not exist." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ProblemDetails() { Detail = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GetProductDTO>> Create([FromForm]CreateProductDTO request)
+        {
+            try
+            {
+                var product = await _productService.AddProduct(request);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ProblemDetails() { Detail = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                await _productService.DeleteProductById(id);
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ProblemDetails() { Detail = ex.Message });
+            }
+        }
+    }
+}
---
+++ API/Services/ProductService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using API.Models.DTOs.Product;
+
+namespace API.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<GetProductDTO> AddProduct(CreateProductDTO request);
+        Task<bool> DeleteProductById(int productId);
+        Task<GetProductDTO?> GetProductById(int productId);
+    }
+}
